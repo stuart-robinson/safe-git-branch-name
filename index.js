e136df2bdd90467d37614cf074a4815803ee9d66
Stuart <stuart@localhost.localdomain>
2020-08-20 15:13:23 +0100

    asda

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ async function run() {
     core.debug(new Date().toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
     await wait(parseInt(ms));
     core.info(new Date().toTimeString());
-    core.info(Object.keys(process.env));
+    core.info(Object.keys(process.env.GITHUB_REF.replace(/\//g, "-")));
 
     core.setOutput("time", new Date().toTimeString());
   } catch (error) {
